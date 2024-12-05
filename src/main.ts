bb28637098da3321c1cff5bd236093b62aac1dad
coline_baudrier <coline.baudrier@outlook.com>
2024-12-05 20:10:42 +0100

    Je répare mes bêtises ça va oui

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,28 @@
 import './assets/main.css'
-
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+
+// Vuetify
+import 'vuetify/styles';
+import { createVuetify } from "vuetify"
+import "@mdi/font/css/materialdesignicons.css"
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
 
 import App from './App.vue'
 import router from './router'
 
+const vuetify = createVuetify({
+  components,
+  directives,
+  icons: {
+    defaultSet: 'mdi',
+  },
+});
+
 const app = createApp(App)
 
+app.use(vuetify)
 app.use(createPinia())
 app.use(router)
 
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import TheWelcome from '../components/TheWelcome.vue'
+import InfoModale from '@/components/InfoModale.vue'
 </script>
 
 <template>
   <main>
+    <InfoModale />
     <TheWelcome />
   </main>
 </template>
