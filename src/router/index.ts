054ba891e27cec36f301963ec83407685beba0ec
jmons <johan.mons@viacesi.fr>
2024-12-06 04:25:49 +0100

    Co-authored-by: Marie <MarieEustace@users.noreply.github.com>

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import Credits from '@/views/CreditsView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,6 +9,11 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: HomeView,
+    },
+    {
+      path: '/credits',
+      name: 'credits',
+      component: Credits,
     },
     {
       path: '/about',
---
+++ src/views/CreditsView.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const titleClass = ref('title')
+</script>
+
+<template>
+  <h1 :class="titleClass">Credits</h1>
+  <img src="../assets/coline.jpg"/>
+</template>
+
+<style>
+.title {
+  text-align: center;
+}
+</style>
